108f8651b36fc1cd26f1b777c6a3fed19bd9ba90
Liquorshotz <jungwon1413@gmail.com>
2017-09-08 18:28:02 +0900

    Update ex6.py

--- Examples/ex6.py
+++ Examples/ex6.py
@@ -10,16 +10,16 @@ print(x)
 print(y)
 print(z)
 
-print("'%s'라고 했어요." % x)
-print("'%s'이라고도 했죠." % y)
-print("So, %r" % z)
+print("'%s'라고 했어요." % x)   # print string
+print("'%s'이라고도 했죠." % y) # print string only
+print("So, %r" % z)            # print raw data, including ' mark
 
 hilarious = False
-joke_evaluation = "웃기는 농담 아니에요?! %r"
+joke_evaluation = "웃기는 농담 아니에요?! %r"            # %r = not for boolean, but for 'raw data'
 
 print(joke_evaluation % hilarious)
 
 w = "이 문자열의 왼쪽->"
-e = "이 문자열의 오른쪽"
+e = "이 문자열의 오른쪽"                 # when you use +, right side comes after the string, before + sign
 
 print(w+e)
