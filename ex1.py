6a4b0085a596996b7217ac23a531ca85a5572919
Liquorshotz <jungwon1413@gmail.com>
2017-09-08 09:25:43 +0900

    example backup

---
+++ ex1.py
@@ -0,0 +1,9 @@
+print("Hello World!")	# no space
+print ("Hello Again")	# one space
+print	("I like typing this.")	# one tab
+print    ("This is fun.")	# 4 spaces
+print('Yay! Printing.')	# no space
+print ("I'd much rather you 'not'.") # one space
+print	('I "said" do not touch this.')	# one tab
+"Hello World!"
+'Hello Again'
