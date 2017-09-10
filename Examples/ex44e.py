acc459638cd764c69d47f182f5db1e73d1ecc451
Liquorshotz <jungwon1413@gmail.com>
2017-09-10 17:10:35 +0900

    Create ex44e.py

---
+++ Examples/ex44e.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+class Other(object):
+
+	def override(self):
+		print("다른 override()")
+
+	def implicit(self):
+		print("다른 implicit()")
+
+	def altered(self):
+		print("다른 altered()")
+
+
+class Child(object):
+
+	def __init__(self):
+		self.other = Other()
+
+	def implicit(self):
+		self.other.implicit()
+
+	def override(self):
+		self.other.override()
+
+	def altered(self):
+		print("자식, 다른 altered() 호출 전")
+		self.other.altered()
+		print("자식, 다른 altered() 호출 후")
+
+son = Child()
+
+son.implicit()
+son.override()
+son.altered()
+
+
+
+# Check out webpage: http://www.python.org/dev/peps/pep-0008/
